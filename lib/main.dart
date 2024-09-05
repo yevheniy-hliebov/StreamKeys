438def4031cd360a2a505dc7740ae246a656aac6
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-09-05 15:33:34 +0300

    install window_manager and set size for window

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:streamkeys/windows/windows_home_page.dart';
 import 'package:streamkeys/windows/server.dart';
+import 'package:window_manager/window_manager.dart';
 
 Future<void> main() async {
   Server.routerHandler();
   await Server.start();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  const size = Size(385, 272);
+
+  const windowOptions = WindowOptions(
+    size: size,
+    maximumSize: size,
+    minimumSize: size,
+    center: true,
+    fullScreen: false,
+    alwaysOnTop: false,
+    title: 'StreamKeys',
+  );
+
+  await windowManager.waitUntilReadyToShow(windowOptions);
+  windowManager.show();
+  windowManager.focus();
+
   runApp(const MyApp());
 }
 
