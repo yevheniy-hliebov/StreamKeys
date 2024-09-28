57da09336fe164a370de15c222bee108cdc9d188
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-09-28 19:35:01 +0300

    update ButtonAction model for Android app and add backroundColor to button

--- lib/android/models/action.dart
+++ lib/android/models/action.dart
@@ -1,12 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:streamkeys/windows/utils/color_helper.dart';
+
 class ButtonAction {
   final int id;
   final String name;
+  final Color backgroundColor;
   final bool hasImage;
   final bool disabled;
 
   const ButtonAction({
     required this.id,
     required this.name,
+    required this.backgroundColor,
     required this.hasImage,
     required this.disabled,
   });
@@ -15,6 +20,7 @@ class ButtonAction {
     return ButtonAction(
       id: json['id'],
       name: json['name'],
+      backgroundColor: ColorHelper.hexToColor(json['backgroundColor']),
       hasImage: json['hasImage'],
       disabled: json['disabled'],
     );
