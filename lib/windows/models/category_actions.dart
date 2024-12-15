a0fc50f7d2e5c439a475f6cd893c6dcf31d749c9
Yevhenii Hliebov <yevheniy.hliebov@gmail.com>
2024-12-15 13:05:57 +0200

    fix: move some actions to toolbox category folder

--- lib/windows/models/category_actions.dart
+++ lib/windows/models/category_actions.dart
@@ -5,8 +5,8 @@ import 'package:streamkeys/windows/models/actions/obs/toogle_mute_source.dart';
 import 'package:streamkeys/windows/models/actions/obs/toogle_visible_source.dart';
 import 'package:streamkeys/windows/models/actions/obs/unmute_source.dart';
 import 'package:streamkeys/windows/models/actions/obs/visible_source.dart';
-import 'package:streamkeys/windows/models/actions/open_app_and_file.dart';
-import 'package:streamkeys/windows/models/actions/website.dart';
+import 'package:streamkeys/windows/models/actions/toolbox/open_app_and_file.dart';
+import 'package:streamkeys/windows/models/actions/toolbox/website.dart';
 import 'package:streamkeys/windows/models/base_action.dart';
 
 class CategoryActions {
