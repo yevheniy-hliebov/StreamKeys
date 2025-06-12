5d698de562e2ab1125771c5837ed624f120051f1
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2025-06-12 18:15:10 +0300

    feat(theme): add AppTabThemeData for tab bar theming

---
+++ lib/core/theme/components/index.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:streamkeys/core/constants/colors.dart';
+
+part 'tab_bar_theme.dart';
---
+++ lib/core/theme/components/tab_bar_theme.dart
@@ -0,0 +1,21 @@
+part of 'index.dart';
+
+class AppTabThemeData {
+  const AppTabThemeData._();
+
+  static TabBarThemeData getTheme(AppColorsData colors) {
+    return TabBarThemeData(
+      dividerColor: Colors.transparent,
+      dividerHeight: 0,
+      labelColor: colors.onPrimary,
+      unselectedLabelColor: colors.onSurface,
+      tabAlignment: TabAlignment.fill,
+      indicatorSize: TabBarIndicatorSize.tab,
+      indicatorColor: Colors.transparent,
+      indicator: BoxDecoration(
+        color: colors.primary,
+      ),
+      // overlayColor: WidgetStatePropertyAll<Color>(colors.primary),
+    );
+  }
+}
