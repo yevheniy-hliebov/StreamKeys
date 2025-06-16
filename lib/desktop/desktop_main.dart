c90f2e11715c0846462144807b8d6177d0e040bb
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2025-06-16 15:47:58 +0300

    feat(deck): move GridDeckScreen and KeyboardDeckSceen into one folder and add DeckLayot to the screens

--- lib/desktop/desktop_main.dart
+++ lib/desktop/desktop_main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:streamkeys/app.dart';
 import 'package:streamkeys/desktop/features/dashboard/presentation/screens/dashboard_screen.dart';
 import 'package:streamkeys/desktop/features/dashboard/presentation/widgets/page_tab.dart';
-import 'package:streamkeys/desktop/features/grid_deck/presentation/screens/grid_deck_screen.dart';
-import 'package:streamkeys/desktop/features/keyboard_deck/presentation/screens/keyboard_deck_screen.dart';
+import 'package:streamkeys/desktop/features/deck/presentation/screens/grid_deck_screen.dart';
+import 'package:streamkeys/desktop/features/deck/presentation/screens/keyboard_deck_screen.dart';
 import 'package:streamkeys/desktop/features/settings/presentation/screens/settings_screen.dart';
 import 'package:streamkeys/service_locator.dart';
 
--- lib/desktop/features/deck/presentation/screens/keyboard_deck_screen.dart
+++ lib/desktop/features/deck/presentation/screens/keyboard_deck_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:streamkeys/desktop/features/dashboard/presentation/widgets/page_tab.dart';
+import 'package:streamkeys/desktop/features/deck/presentation/widgets/deck_layout.dart';
 
 class KeyboardDeckScreen extends StatelessWidget with PageTab {
   const KeyboardDeckScreen({super.key});
@@ -15,8 +16,19 @@ class KeyboardDeckScreen extends StatelessWidget with PageTab {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(label),
+    return DeckLayout(
+      leftSide: Container(
+        color: Colors.blue,
+      ),
+      rightSide: Container(
+        color: Colors.yellow,
+      ),
+      mainTop: Container(
+        color: Colors.green,
+      ),
+      mainBottom: Container(
+        color: Colors.red,
+      ),
     );
   }
 }
