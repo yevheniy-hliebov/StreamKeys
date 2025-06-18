f55a88bdf003246da33b95cf715e5bed12e2daa2
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2025-06-18 11:34:38 +0300

    feat(deck-screens): integrate KeyGridArea into GridDeckScreen and KeyboardDeckScreen

--- lib/desktop/features/deck/presentation/screens/keyboard_deck_screen.dart
+++ lib/desktop/features/deck/presentation/screens/keyboard_deck_screen.dart
@@ -3,7 +3,9 @@ import 'package:streamkeys/core/constants/colors.dart';
 import 'package:streamkeys/desktop/features/dashboard/presentation/widgets/page_tab.dart';
 import 'package:streamkeys/desktop/features/deck/presentation/widgets/deck_layout.dart';
 import 'package:streamkeys/desktop/features/deck_page_list/bloc/deck_page_list_bloc.dart';
+import 'package:streamkeys/desktop/features/deck_page_list/data/models/deck_type.dart';
 import 'package:streamkeys/desktop/features/deck_page_list/presentation/widgets/deck_page_list.dart';
+import 'package:streamkeys/desktop/features/key_grid_area/presentation/widgets/key_grid_area.dart';
 
 class KeyboardDeckScreen extends StatelessWidget with PageTab {
   const KeyboardDeckScreen({super.key});
@@ -24,9 +26,7 @@ class KeyboardDeckScreen extends StatelessWidget with PageTab {
       rightSide: Container(
         color: AppColors.of(context).surface,
       ),
-      mainTop: Container(
-        color: AppColors.of(context).background,
-      ),
+      mainTop: const KeyGridArea(deckType: DeckType.keyboard),
       mainBottom: Container(
         color: AppColors.of(context).surface,
       ),
