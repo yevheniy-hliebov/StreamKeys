d6aac8e88f132e7fb4b00b0f5bf8315899f76d5b
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2025-06-22 16:31:22 +0300

    feat(key-editor): add KeyEditorHeader, KeyEditorLoader, and KeyEditorPlaceholder widgets

---
+++ lib/desktop/features/key_bindings/presentation/widgets/key_editor_placeholder.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:streamkeys/core/constants/typography.dart';
+
+class KeyEditorPlaceholder extends StatelessWidget {
+  const KeyEditorPlaceholder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Select a key button',
+        style: AppTypography.subtitle,
+      ),
+    );
+  }
+}
