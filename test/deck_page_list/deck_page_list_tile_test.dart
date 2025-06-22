d6bd5cc66e0bc369107a57c9e204d133b4dbe898
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2025-06-22 10:00:24 +0300

    refactor(tests): update DeckPageListTile tests to use DeckPage model

--- test/deck_page_list/deck_page_list_tile_test.dart
+++ test/deck_page_list/deck_page_list_tile_test.dart
@@ -1,22 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:streamkeys/desktop/features/deck_page_list/data/models/deck_page.dart';
 import 'package:streamkeys/desktop/features/deck_page_list/presentation/widgets/deck_page_list_tile.dart';
 
 void main() {
+  final testPage = DeckPage(id: '1', name: 'Page 1');
+
   group('DeckPageListTile', () {
     testWidgets('shows Text when not editing or not current',
         (WidgetTester tester) async {
       await tester.pumpWidget(
-        const MaterialApp(
+        MaterialApp(
           home: DeckPageListTile(
-            pageName: 'Page 1',
+            page: testPage,
             isCurrent: false,
             isEditing: false,
           ),
         ),
       );
 
-      expect(find.text('Page 1'), findsOneWidget);
+      expect(find.text(testPage.name), findsOneWidget);
       expect(find.byType(TextFormField), findsNothing);
     });
 
@@ -25,7 +28,7 @@ void main() {
       await tester.pumpWidget(
         MaterialApp(
           home: DeckPageListTile(
-            pageName: 'Page 1',
+            page: testPage,
             isCurrent: true,
             isEditing: true,
             onStopEditing: (String newName) {},
@@ -34,7 +37,7 @@ void main() {
       );
 
       expect(find.byType(TextFormField), findsOneWidget);
-      expect(find.text('Page 1'), findsOneWidget);
+      expect(find.text(testPage.name), findsOneWidget);
     });
 
     testWidgets('calls onSelect when tapped', (WidgetTester tester) async {
@@ -42,7 +45,7 @@ void main() {
       await tester.pumpWidget(
         MaterialApp(
           home: DeckPageListTile(
-            pageName: 'Page 1',
+            page: testPage,
             onSelect: () {
               tapped = true;
             },
@@ -60,7 +63,7 @@ void main() {
       await tester.pumpWidget(
         MaterialApp(
           home: DeckPageListTile(
-            pageName: 'Page 1',
+            page: testPage,
             isCurrent: true,
             isEditing: true,
             onStopEditing: (String newName) {
