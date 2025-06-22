daa1fc83740ccd7c1807b3d2702b65ec851236d3
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2025-06-22 11:17:37 +0300

    refactor(key-bindings): make KeyBindingData immutable, add copyWith and Equatable

--- lib/desktop/features/key_bindings/data/models/key_binding_data.dart
+++ lib/desktop/features/key_bindings/data/models/key_binding_data.dart
@@ -1,16 +1,29 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:streamkeys/desktop/utils/color_helper.dart';
 
-class KeyBindingData {
-  String name;
-  Color? backgroundColor;
-  String imagePath;
+class KeyBindingData extends Equatable {
+  final String name;
+  final Color? backgroundColor;
+  final String imagePath;
 
-  KeyBindingData({
+  const KeyBindingData({
     this.name = '',
     this.backgroundColor,
     this.imagePath = '',
   });
+
+  KeyBindingData copyWith({
+    String? name,
+    Color? backgroundColor,
+    String? imagePath,
+  }) {
+    return KeyBindingData(
+      name: name ?? this.name,
+      backgroundColor: backgroundColor ?? this.backgroundColor,
+      imagePath: imagePath ?? this.imagePath,
+    );
+  }
 
   factory KeyBindingData.fromJson(Map<String, dynamic> json) {
     return KeyBindingData(
@@ -27,6 +40,9 @@ class KeyBindingData {
       'image_path': imagePath,
     };
   }
+
+  @override
+  List<Object?> get props => [name, backgroundColor, imagePath];
 }
 
 typedef KeyBindingMap = Map<String, KeyBindingData>;
