9b5595bb73622e2b811de6c87b13ddb084a700bf
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2025-06-27 22:30:35 +0300

    feat(server): add key click endpoints and implement base controller for error handling

--- lib/desktop/server/api_router.dart
+++ lib/desktop/server/api_router.dart
@@ -1,11 +1,20 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
+import 'package:streamkeys/desktop/features/deck_page_list/data/models/deck_type.dart';
+import 'package:streamkeys/desktop/server/controllers/key_controller.dart';
 
 class ApiRouter {
   final _router = Router();
+  final _keyController = KeyController();
 
   ApiRouter() {
     _router.get('/', (Request request) => Response.ok('API Worked!'));
+    _router.get('/grid/<index>/click', (Request request, String keyCode) {
+      return _keyController.clickKey(request, keyCode, DeckType.grid);
+    });
+    _router.get('/keyboard/<index>/click', (Request request, String keyCode) {
+      return _keyController.clickKey(request, keyCode, DeckType.keyboard);
+    });
   }
 
   Router get router => _router;
---
+++ lib/desktop/server/controllers/key_controller.dart
@@ -0,0 +1,42 @@
+import 'package:shelf/shelf.dart';
+import 'package:streamkeys/desktop/features/deck_page_list/data/models/deck_json_keys.dart';
+import 'package:streamkeys/desktop/features/deck_page_list/data/models/deck_type.dart';
+import 'package:streamkeys/desktop/features/key_bindings/data/models/key_binding_data.dart';
+import 'package:streamkeys/desktop/server/controllers/base_controller.dart';
+import 'package:streamkeys/desktop/utils/local_json_file_manager.dart';
+
+class KeyController extends BaseController {
+  Future<Response> clickKey(
+    Request request,
+    String keyCode,
+    DeckType dekcType,
+  ) async {
+    final jsonHelper = LocalJsonFileManager.storage(
+      '${dekcType.name}_deck.json',
+    );
+
+    try {
+      final json = await jsonHelper.read();
+
+      if (json == null) {
+        return Response.internalServerError(body: 'Failed to read deck json');
+      }
+
+      final currentPageId = json[DeckJsonKeys.currentPageId];
+      final keyBindingDataJson = json[DeckJsonKeys.map][currentPageId][keyCode];
+
+      if (keyBindingDataJson == null) {
+        return Response.badRequest(body: 'Key binding data not found');
+      }
+
+      final keyBindingData = KeyBindingData.fromJson(keyBindingDataJson);
+      for (final action in keyBindingData.actions) {
+        await action.execute();
+      }
+
+      return Response.ok('Key actions executed successfully');
+    } catch (e) {
+      return handleError(e);
+    }
+  }
+}
