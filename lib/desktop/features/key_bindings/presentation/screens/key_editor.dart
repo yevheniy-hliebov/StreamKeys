fe6124a66ecfe0157bcb4c0cb99dbecca8bdce37
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2025-06-27 14:33:39 +0300

    refactor(key-editor): integrate KeyBindingActionList and KeyBindingActionTile for improved action management

--- lib/desktop/features/key_bindings/presentation/screens/key_editor.dart
+++ lib/desktop/features/key_bindings/presentation/screens/key_editor.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:streamkeys/core/constants/colors.dart';
 import 'package:streamkeys/desktop/features/deck/presentation/widgets/deck_devider.dart';
 import 'package:streamkeys/desktop/features/key_bindings/bloc/key_bindings_bloc.dart';
+import 'package:streamkeys/desktop/features/key_bindings/presentation/widgets/key_binding_action_list.dart';
 import 'package:streamkeys/desktop/features/key_bindings/presentation/widgets/key_editor_loader.dart';
 import 'package:streamkeys/desktop/features/key_bindings/presentation/widgets/key_editor_placeholder.dart';
 import 'package:streamkeys/desktop/features/key_bindings/presentation/widgets/key_setting_panel.dart';
@@ -17,22 +18,22 @@ class KeyEditor<T extends KeyBindingsBloc> extends StatelessWidget {
       child: BlocBuilder<T, KeyBindingsState>(
         builder: (context, state) {
           if (state is KeyBindingsLoaded) {
-            if (state.currentKeyData == null) {
+            if (state.currentKeyData == null || state.currentKeyBindingData == null) {
               return const KeyEditorPlaceholder();
             }
 
             final keyData = state.currentKeyData!;
+            final keyBindingData = state.currentKeyBindingData!;
             final bloc = context.read<T>();
-            final keyBindingData = bloc.getKeyBindingData(keyData.keyCode);
 
             return Row(
-              key: Key('${keyData.keyCode}-${keyBindingData.id}'),
               children: [
                 ConstrainedBox(
                   constraints: const BoxConstraints(
                     maxWidth: 318,
                   ),
                   child: KeySettingPanel(
+                    key: Key(keyBindingData.id),
                     keyData: keyData,
                     keyBindingData: keyBindingData,
                     onClearPressed: () {
@@ -62,7 +63,24 @@ class KeyEditor<T extends KeyBindingsBloc> extends StatelessWidget {
                   ),
                 ),
                 const DeckDevider(axis: Axis.vertical),
-                const Flexible(child: SizedBox()), // stub for actions
+                Flexible(
+                  child: KeyBindingActionList(
+                    actions: keyBindingData.actions,
+                    onDeleteActionPressed: (index) {
+                      bloc.add(KeyBindingsDeleteAction(
+                        keyData.keyCode,
+                        index,
+                      ));
+                    },
+                    onReorder: (oldIndex, newIndex) {
+                      bloc.add(KeyBindingsReorderActions(
+                        keyCode: keyData.keyCode,
+                        oldIndex: oldIndex,
+                        newIndex: newIndex,
+                      ));
+                    },
+                  ),
+                ),
               ],
             );
           } else {
---
+++ lib/desktop/features/key_bindings/presentation/widgets/key_binding_action_list.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:streamkeys/common/widgets/field_label.dart';
+import 'package:streamkeys/core/constants/spacing.dart';
+import 'package:streamkeys/desktop/features/action_library/data/models/binding_action.dart';
+import 'package:streamkeys/desktop/features/key_bindings/presentation/widgets/binding_action_tile.dart';
+import 'package:streamkeys/desktop/features/key_bindings/presentation/widgets/key_binding_actions_container.dart';
+
+class KeyBindingActionList extends StatelessWidget {
+  final List<BindingAction> actions;
+  final void Function(int index)? onDeleteActionPressed;
+  final void Function(int oldIndex, int newIndex)? onReorder;
+
+  const KeyBindingActionList({
+    super.key,
+    required this.actions,
+    this.onDeleteActionPressed,
+    this.onReorder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(Spacing.sm),
+      child: Column(
+        spacing: Spacing.xs,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const FieldLabel('Actions'),
+          Flexible(
+            child: KeyBindingActionsContainer(
+              child: ReorderableListView.builder(
+                buildDefaultDragHandles: false,
+                itemCount: actions.length,
+                onReorder: (oldIndex, newIndex) {
+                  onReorder?.call(oldIndex, newIndex);
+                },
+                itemBuilder: (context, index) {
+                  final action = actions[index];
+                  return ReorderableDragStartListener(
+                    key: Key('$index-${action.actionLabel}'),
+                    index: index,
+                    child: KeyBindingActionTile(
+                      action: action,
+                      onDeletePressed: () {
+                        onDeleteActionPressed?.call(index);
+                      },
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
