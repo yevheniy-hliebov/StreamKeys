683a1023d298c817cbd82685a49bf615f1fc1e07
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2025-06-30 10:15:51 +0300

    feat(hidmacros): add KeyboardPreferences class for managing keyboard settings with shared preferences

---
+++ lib/desktop/features/hidmacros/data/services/hidmacros_preferences.dart
@@ -0,0 +1,43 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:streamkeys/desktop/features/hidmacros/data/models/keyboard_device.dart';
+import 'package:streamkeys/desktop/features/key_grid_area/data/models/keyboard_type.dart';
+
+class KeyboardPreferences {
+  final _systemIdKey = 'system_id';
+  final _keyboardNameKey = 'keyboard_name';
+  final _keyboardTypeKey = 'keyboard_type';
+
+  Future<void> saveSelectedKeyboard(KeyboardDevice keyboard) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_systemIdKey, Uri.encodeComponent(keyboard.systemId));
+    await prefs.setString(_keyboardNameKey, keyboard.name);
+  }
+
+  Future<KeyboardDevice?> getSelectedKeyboard() async {
+    final prefs = await SharedPreferences.getInstance();
+    final encodedId = prefs.getString(_systemIdKey) ?? '';
+    final name = prefs.getString(_keyboardNameKey) ?? '';
+    if (encodedId.isEmpty || name.isEmpty) return null;
+
+    final id = Uri.decodeComponent(encodedId);
+    return KeyboardDevice(name, id);
+  }
+
+  Future<void> saveKeyboardType(KeyboardType type) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_keyboardTypeKey, type.name);
+  }
+
+  Future<KeyboardType?> getKeyboardType() async {
+    final prefs = await SharedPreferences.getInstance();
+    final type = prefs.getString(_keyboardTypeKey);
+
+    for (var keyboardType in KeyboardType.values) {
+      if (keyboardType.name == type) {
+        return keyboardType;
+      }
+    }
+
+    return null;
+  }
+}
