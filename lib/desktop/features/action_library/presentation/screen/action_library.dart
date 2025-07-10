1df0cfc77891124cd1a6d2e63bba570f0c84e086
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2025-07-10 17:01:36 +0300

    fix(action-library): isolate Expansible controller by moving into separate widget to prevent shared state bug

--- lib/desktop/features/action_library/presentation/screen/action_library.dart
+++ lib/desktop/features/action_library/presentation/screen/action_library.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:streamkeys/common/widgets/helpers/for.dart';
-import 'package:streamkeys/core/constants/colors.dart';
-import 'package:streamkeys/desktop/features/action_library/data/models/binding_action.dart';
 import 'package:streamkeys/desktop/features/action_library/data/models/binding_action_category.dart';
-import 'package:streamkeys/desktop/features/action_library/presentation/widgets/binding_action_category_tile.dart';
-import 'package:streamkeys/desktop/features/action_library/presentation/widgets/dragable_binding_action_tile.dart';
+import 'package:streamkeys/desktop/features/action_library/presentation/widgets/category_expansion_tile.dart';
 import 'package:streamkeys/desktop/features/deck/presentation/widgets/deck_devider.dart';
 
 class ActionLibrary extends StatelessWidget {
@@ -13,51 +10,13 @@ class ActionLibrary extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final library = BindingActionCategory.library;
-    final controller = ExpansibleController();
 
     return Column(
       children: For.fromList<BindingActionCategory>(
         items: library,
         generator: (category) {
           return [
-            Expansible(
-              controller: controller,
-              headerBuilder: (context, animation) {
-                return BindingActionCategoryTile(
-                  category: category,
-                  onTap: () {
-                    if (controller.isExpanded) {
-                      controller.collapse();
-                    } else {
-                      controller.expand();
-                    }
-                  },
-                );
-              },
-              bodyBuilder: (context, animation) {
-                return Container(
-                  decoration: BoxDecoration(
-                    color: AppColors.of(context).background,
-                    border: Border(
-                      top: BorderSide(
-                        color: AppColors.of(context).outlineVariant,
-                        width: 4,
-                      ),
-                    ),
-                  ),
-                  child: Column(
-                    children: For.fromList<BindingAction>(
-                      items: category.actions,
-                      generator: (action) {
-                        return [
-                          DragableBindingActionTile(bindingAction: action),
-                        ];
-                      },
-                    ),
-                  ),
-                );
-              },
-            ),
+            CategoryExpansionTile(category: category),
             const DeckDevider(axis: Axis.horizontal),
           ];
         },
