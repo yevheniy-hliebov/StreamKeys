31439abfefa4d541ff7c38dc7f2069f142ca8b06
yevheniy-hliebov <yevheniy.hliebov@gmail.com>
2025-09-15 14:50:29 +0300

    chore: update app version to 3.1.2 in version.dart and pubspec.yaml

--- lib/core/constants/version.dart
+++ lib/core/constants/version.dart
@@ -1,6 +1,6 @@
 import 'package:github_updater/github_updater.dart';
 
 class AppVersion {
-  static const String appVersion = 'v3.1.1';
+  static const String appVersion = 'v3.1.2';
   static const AppVersionMode mode = AppVersionMode.stable;
 }
